a40c8aaab511232d3845bb89b67f278434897a88
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-02-28 22:30:27 +0530

    scraping amazon

---
+++ bottle.py
@@ -0,0 +1,19 @@
+ACCESS_KEY_ID = "AKIAJHZCPV6PDEQXFQLQ"
+SECRET_KEY = "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"
+ASSOC_TAG = "pst037-20"
+
+import bottlenose
+from lxml import objectify
+from amazon.api import AmazonAPI
+import amazon_scraper
+
+amazon = AmazonAPI(ACCESS_KEY_ID, SECRET_KEY, ASSOC_TAG)
+products = amazon.search_n(1, Keywords='Moto X Pure', SearchIndex='All')
+#products=products[0:10]
+asinlist=[]
+for product in products:
+	asinlist.append(product.asin)
+
+#amazon.call(asinlist[0])
+amazon_scraper.ReadAsin(asinlist)
+

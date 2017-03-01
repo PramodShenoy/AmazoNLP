412c879c3773eedfff360ed9fc28798f1dbf5120
Diptark Bose <diptark.bose@gmail.com>
2017-03-01 19:41:35 +0530

    Add files via upload

---
+++ senti_analysis_per_product.py
@@ -0,0 +1,36 @@
+from textblob import TextBlob
+from nltk import sent_tokenize
+
+#repeat this code snippet for 5 times for top 5 products obtained
+
+review_list=[]
+n=0
+review_count=0
+total=[]
+
+for i in review_list:
+	review=sent_tokenize(i)
+	pos=[]
+	count=0
+	for line in review:
+		line=TextBlob(line)
+		count=count+1
+		pos.append(line.sentiment.polarity)
+
+	polar_mean=0
+	for i in pos:
+		polar_mean=polar_mean+i
+	polar_mean=polar_mean/count
+
+	print ("Positivity for review"+ str(n+1)+ "= " + str(polar_mean*100) +"%")
+	total.append(polar_mean*100)
+	n=n+1
+	#will introduce parameters for positive, negative, neutral soon
+	#will include subjectivity
+
+	#textblob makes it so easy!!!
+
+for i in total:
+	overall_score=worth_buy+i
+overall_score=overall_score/(n+1)
+print("Overall user satisfaction for this product: "+ str(overall_score))

707d88727742dd2f3dedc7cc3df265a210ca2491
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-03-01 20:28:15 +0530

    Update bottle.py

--- bottle.py
+++ bottle.py
@@ -6,15 +6,35 @@ import bottlenose
 from lxml import objectify
 from amazon.api import AmazonAPI
 import amazon_scraper
+import json
+from pprint import pprint
 
 amazon = AmazonAPI(ACCESS_KEY_ID, SECRET_KEY, ASSOC_TAG)
-search_item=raw_input()
+#search_item=raw_input()
+search_item='moto z'
 products = amazon.search_n(5, Keywords=search_item, SearchIndex='All')
 #products=products[0:10]
 asinlist=[]
 for product in products:
 	asinlist.append(product.asin)
+	print product.title
 
 #amazon.call(asinlist[0])
 amazon_scraper.ReadAsin(asinlist)
 
+data=open('data.json','r')
+reviews = json.loads(data.read())
+data.close()
+#pprint(reviews)
+#a=reviews[1]['reviews'][0]
+#print a['review_text']
+review_list=[]
+a=reviews[0]['reviews'][0]
+for i in range(5):
+	for j in range(len(a)):
+		a=reviews[i]['reviews'][j]
+		review_list.append(a['review_text'])
+count=1
+for line in review_list:
+	print count,line
+	count=count+1
